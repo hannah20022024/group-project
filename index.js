29e6aa802723c4e92bf7959ff2f61a6f7c7cb2a8
99219 <992190862@qq.com>
2025-07-29 11:17:05 +0800

    cash amount

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express';
-// import indexRouter from './routes/index.js';
+import cashRouter from './routes/index.js';
 import pool from './config/db.js';
+import yaml from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
+app.use(express.json());
+
+const swaggerDocument = yaml.load('./swagger.yaml');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+console.log("load success");
 
 async function start() {
   try {
@@ -10,8 +18,13 @@ async function start() {
     const [rows] = await pool.query('SELECT * from holdings');
     console.log('✅ MySQL 连接成功:', rows);
 
-    // Use Router
-    // app.use('/', indexRouter);
+    // // Use Router
+    app.use('/api', cashRouter);
+
+    // cons
+    // app.get('/api/getCash', (req, res) => {
+    //   console.log("login ")
+    // });
 
     app.listen(3000, () => {
       console.log('🚀 服务器启动：http://localhost:3000');
